9c6a68bfaa96649b59f9da333196359c9bbd08e2
zakary dujardin <zakoduj@gmail.com>
2023-02-12 15:49:47 -0500

    fix connection

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,11 +13,6 @@ export default {
   props: {
     msg: String
   },
-  mounted() {
-    const SPOTIFY_CLIENT_ID = 'ba07904c005743fc9b90cb3e6784ea04';
-    const SPOTIFY_REDIRECT_URI ='http://localhost:8080/callback';
-    window.location.href = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${SPOTIFY_REDIRECT_URI}`;
-  }
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
