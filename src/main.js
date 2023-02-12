4984901588c7769ea5016dda2ca835c9a757cc1e
zakary dujardin <zakoduj@gmail.com>
2023-02-12 10:29:04 -0500

    rebase before dotenv

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import dotenv from'dotenv'
 
 createApp(App).mount('#app')
 
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-const Dotenv = require('dotenv-webpack');
-
 module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {
-    resolve: {
-      fallback: {
-        "path": require.resolve("path-browserify"),
-        "os": require.resolve("os-browserify/browser")
-      }
-    },
-    plugins: [
-      new Dotenv()
-    ]
-  }
+  transpileDependencies: true
 })
