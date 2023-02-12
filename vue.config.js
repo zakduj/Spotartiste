f02a69b77a79fac83cb85b7a17f78dfd38fa8784
zakary dujardin <zakoduj@gmail.com>
2023-02-12 10:13:33 -0500

    fix dotenv

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "path": require.resolve("path-browserify"),
+        "os": require.resolve("os-browserify/browser")
+      }
+    }
+  }
+}
