50f99ae71e065b9f6f36e7b35dfbec0e11ab440f
zakary dujardin <zakoduj@gmail.com>
2023-02-12 22:03:23 -0500

    feature utilisation de acces token

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,24 +2,58 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <form>
-    <input type="text" placeholder="Rechercher...">
-    <button type="submit">Go</button>
+      <input type="text" placeholder="Rechercher..." v-model="searchTerm">
+      <button type="submit" @click="searchArtists">Go</button>
     </form>
-      <button @click="login">Login</button>
+    <div v-if="artists.length">
+      <h2>Results:</h2>
+      <ul>
+        <li v-for="artist in artists" :key="artist.id">{{ artist.name }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
+  data() {
+    return {
+      searchTerm: '',
+      artists: [],
+      accessToken: ''
+    }
+  },
   methods: {
-    login() {
+    async searchArtists() {
+      try {
+        if (!this.accessToken) {
+          await this.getAccessToken();
+        }
+        const res = await axios.get(`https://api.spotify.com/v1/search?q=${this.searchTerm}&type=artist`, {
+          headers: {
+            Authorization: `Bearer ${this.accessToken}`
+          }
+        });
+        this.artists = res.data.artists.items;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async getAccessToken() {
       const SPOTIFY_CLIENT_ID = 'ba07904c005743fc9b90cb3e6784ea04';
-      const SPOTIFY_REDIRECT_URI ='http://localhost:8080/callback';
-      window.location.href = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${SPOTIFY_REDIRECT_URI}`;
+      const SPOTIFY_REDIRECT_URI = 'http://localhost:8080/callback';
+      const res = await axios.post('https://accounts.spotify.com/api/token', {
+        grant_type: 'client_credentials',
+        client_id: SPOTIFY_CLIENT_ID,
+        client_secret: SPOTIFY_CLIENT_SECRET
+      });
+      this.accessToken = res.data.access_token;
     }
   }
 }
